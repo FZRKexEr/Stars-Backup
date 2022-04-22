c31054ee7b3b7ae35167895a01e36c77fd2dfc52
LiZnB <wrong.answer@qq.com>
2022-04-22 15:31:12 +0800

    Update

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ from git import Repo
 def sol(content):
 	# 格式化 json, 类型是 str
 	# print(json.dumps(content, indent=2))
-	address = []
 	for i in content:
-		address.append(i['clone_url'])
-
+		repo = Repo.clone_from(url=i['clone_url'], to_path='../github_stars/' + i['name'])
 
 def main():
 	user = 'fzrkexer'
-	star = requests.get('https://api.github.com/users/' + user + '/starred')
-	if star.ok:
-		# json.loads() 把 star.content 转换成 list
-		sol(json.loads(star.content))
-	else:
-		print(star.raise_for_status())
+	for i in range(100):
+		star = requests.get('https://api.github.com/users/' + user + '/starred?page=' + str(i))
+		if len(json.loads(star.content)) == 0:
+			break
+		if star.ok:
+			# json.loads() 把 star.content 转换成 list
+			sol(json.loads(star.content))
+		else:
+			print(star.raise_for_status())
 
 if __name__ == '__main__':
 	main()
+
